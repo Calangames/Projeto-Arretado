a3611ffaf71319b7d6da9f8b856f3a616f3419c4
wesleyronald <wesleyronaldma@gmail.com>
2019-12-02 20:40:56 -0300

    improved movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,13 @@ public class Player : MonoBehaviour
 {
     [Range(0f, 40f)]
     public float speed = 6f;
+    public AnimationCurve decelerationRate;
     public string[] collidersTagArray = new string[1] { "Solid"};
 
     public static Player instance = null;
-
-    private float horizontalMovement, verticalMovement;
+    private Vector2 movement;
+    private float oldHInput, oldVInput, hInput, vInput;
+    private bool movingH, movingV;
     private CollisionController3D _collisionController;
 
     void Awake()
@@ -28,8 +30,62 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {        
-        horizontalMovement = Input.GetAxisRaw("Horizontal");
-        verticalMovement = Input.GetAxisRaw("Vertical");
-        _collisionController.CalculateMovement(horizontalMovement, verticalMovement, speed);
+        if (Input.GetAxisRaw("Horizontal") == 0f)
+        {
+            if (movingH)
+            {
+                oldHInput = hInput;
+                StopCoroutine(HRelease());
+                StartCoroutine(HRelease());
+            }
+            movingH = false;
+        }
+        else
+        {
+            hInput = Input.GetAxisRaw("Horizontal");            
+            movingH = true;            
+        }
+
+        if (Input.GetAxisRaw("Vertical") == 0f)
+        {
+            if (movingV)
+            {
+                oldVInput = vInput;
+                StopCoroutine(VRelease());
+                StartCoroutine(VRelease());
+            }
+            movingV = false;
+        }
+        else
+        {
+            vInput = Input.GetAxisRaw("Vertical");
+            movingV = true;
+        }
+
+        Vector2 normalized = new Vector2(Mathf.Abs(hInput), Mathf.Abs(vInput)).normalized;
+        movement = new Vector2(hInput, vInput) * normalized;
+        _collisionController.CalculateMovement(movement.x, movement.y, speed);
     } 
+
+    private IEnumerator HRelease()
+    {
+        float time = 0f;        
+        while (time < decelerationRate.keys[decelerationRate.length - 1].time)
+        {
+            time += Time.deltaTime;
+            hInput = oldHInput * decelerationRate.Evaluate(time);            
+            yield return null;
+        }        
+    }
+
+    private IEnumerator VRelease()
+    {
+        float time = 0f;
+        while (time < decelerationRate.keys[decelerationRate.length - 1].time)
+        {
+            time += Time.deltaTime;
+            vInput = oldVInput * decelerationRate.Evaluate(time);
+            yield return null;
+        }
+    }
 }
